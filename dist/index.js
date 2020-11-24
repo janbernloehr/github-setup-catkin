7ddbb52b5232c1869a733bb267fd040817632ea2
janbernloehr <jan@bernloehrs.de>
2020-11-24 16:45:13 +0100

    Set ACTIONS_ALLOW_UNSECURE_COMMANDS

--- dist/index.js
+++ dist/index.js
@@ -145,7 +145,7 @@ const core = __importStar(__webpack_require__(470));
 const finder = __importStar(__webpack_require__(629));
 const child_process = __webpack_require__(129);
 function installRos(version) {
-    let command = `sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
+    let command = `export ACTIONS_ALLOW_UNSECURE_COMMANDS=true && sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 &&
 sudo apt-get update &&
 sudo apt-get -qq update -y &&
